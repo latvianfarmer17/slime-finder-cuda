60701045d2a72997020d96cc886a94fa91736391
latvianfarmer17 <58879242+latvianfarmer17@users.noreply.github.com>
2024-06-27 17:00:27 +0100

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,22 @@
+@echo off
+
+IF NOT EXIST "bin" MKDIR bin
+
+REM Compile DLL
+echo [!] Compiling 'SlimeFinder.dll'
+nvcc --shared -o bin/SlimeFinder.dll src/SlimeFinder.cu
+
+IF %ERRORLEVEL% NEQ 0 (
+	exit /b %ERRORLEVEL%
+)
+
+REM Compile program with linked lib.dll
+echo [!] Compiling 'SlimeFinder.exe'
+g++ -o bin/SlimeFinder.exe -L. -l bin/SlimeFinder src/main.cpp src/UserIO.cpp
+
+IF %ERRORLEVEL% NEQ 0 (
+	exit /b %ERRORLEVEL%
+) ELSE (
+	echo [!] Built 'SlimeFinder.exe' successfully...
+	pause
+)
