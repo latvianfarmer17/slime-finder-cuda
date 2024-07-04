816dab420e443ca2756f871ce787bf87da0001be
latvianfarmer17 <58879242+latvianfarmer17@users.noreply.github.com>
2024-07-04 13:54:04 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,14 +7,16 @@ echo [!] Compiling 'SlimeFinder.dll'
 nvcc --shared -o bin/SlimeFinder.dll src/SlimeFinder.cu
 
 IF %ERRORLEVEL% NEQ 0 (
+	pause
 	exit /b %ERRORLEVEL%
 )
 
 REM Compile program with linked lib.dll
 echo [!] Compiling 'SlimeFinder.exe'
-g++ -o bin/SlimeFinder.exe -L. -l bin/SlimeFinder src/main.cpp src/UserIO.cpp
+g++ -O3 -o bin/SlimeFinder.exe -L. -l bin/SlimeFinder src/main.cpp src/UserIO.cpp
 
 IF %ERRORLEVEL% NEQ 0 (
+	pause
 	exit /b %ERRORLEVEL%
 ) ELSE (
 	echo [!] Built 'SlimeFinder.exe' successfully...
